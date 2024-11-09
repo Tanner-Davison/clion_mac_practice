9125d74e133c8ca02acf31c852f603659774be65
Tanner Davison <tanner.davison95@gmail.com>
2024-11-08 22:01:16 -0700

    pushing

--- main.cpp
+++ main.cpp
@@ -28,13 +28,3 @@ int main() {
     return 0;
 }
 
-/*
- //const int* const ptr = &x;
-//  ^      ^
-//  |      |
-//  |      const pointer (CAN'T reassign)
-//  const int (CAN'T modify value)
-
-// This has BOTH:
-// 1. Low-level const (const int) - can't modify value
-// 2. High-level const (const ptr) - can't reassign pointer*/
