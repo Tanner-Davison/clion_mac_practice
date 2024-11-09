65ea97957624ab50e17b504c28369bf41d45fb97
Tanner Davison <tanner.davison95@gmail.com>
2024-11-08 22:14:20 -0700

    reverting back to old

--- main.cpp
+++ main.cpp
@@ -1,30 +1,61 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <cctype>
+#include <fstream>
 
 using namespace std;
 
 int main() {
 
-    char ca[]={'c','h','a','r','\0'};
-    char ca2[]={'a','r','r','a','y','\0'};
-
-    size_t csize = (strlen(ca)+1) + (strlen(ca2)+1);
-    char combined[csize];
-
-
-    if(sizeof(combined) >= csize)
+    fstream inFile("input.txt");
+    if(!inFile)
     {
-        strcpy(combined, ca);
-        strcat(combined, " ");
-        strcat(combined, ca2);
+        cout << "No input file found!" << endl;
+        return 0;
     }
-    char *p = combined;
-    while(*p)
+    string input_string;
+    vector<string> result_vector;
+
+    int index = 0;
+    while(getline(inFile, input_string) && !input_string.empty())
     {
-        cout << *p;
-        ++p;
+       if(!input_string.empty())
+       {
+           string temp_word;
+                for(const auto& c : input_string)
+                    {
+                    if(!isspace(c))
+                        temp_word += static_cast<char>(toupper(c));
+                    else if(!temp_word.empty())
+                    {
+                        result_vector.emplace_back(temp_word);
+                        temp_word.clear();
+                    }
+           }
+           if(!temp_word.empty())
+                result_vector.emplace_back(temp_word);
+       }
     }
-    cout << endl;
+
+    for(const auto& word: result_vector)
+    {
+        if(index > 0 && index % 9 ==0)
+            cout << "\n";
+        cout << word << " ";
+        ++index;
+    }
+    cout << '\n';
     return 0;
 }
 
+/*
+ //const int* const ptr = &x;
+//  ^      ^
+//  |      |
+//  |      const pointer (CAN'T reassign)
+//  const int (CAN'T modify value)
+
+// This has BOTH:
+// 1. Low-level const (const int) - can't modify value
+// 2. High-level const (const ptr) - can't reassign pointer*/
