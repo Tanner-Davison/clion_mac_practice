47da536c0ac56add6399575a9b29e36dadd24862
Tanner Davison <tanner.davison95@gmail.com>
2024-11-08 22:21:24 -0700

    dereferencing a vector<Person*>::iterator it = people.begin();

--- main.cpp
+++ main.cpp
@@ -6,56 +6,25 @@
 
 using namespace std;
 
-int main() {
+struct Person
+{
+    string name;
+    int age;
+};
 
-    fstream inFile("input.txt");
-    if(!inFile)
+int main()
+{
+    vector<Person*> people;
+    people.emplace_back(new Person("tanner", 28));
+    for(auto it = people.begin(); it < people.end(); ++it)
     {
-        cout << "No input file found!" << endl;
-        return 0;
-    }
-    string input_string;
-    vector<string> result_vector;
-
-    int index = 0;
-    while(getline(inFile, input_string) && !input_string.empty())
-    {
-       if(!input_string.empty())
-       {
-           string temp_word;
-                for(const auto& c : input_string)
-                    {
-                    if(!isspace(c))
-                        temp_word += static_cast<char>(toupper(c));
-                    else if(!temp_word.empty())
-                    {
-                        result_vector.emplace_back(temp_word);
-                        temp_word.clear();
-                    }
-           }
-           if(!temp_word.empty())
-                result_vector.emplace_back(temp_word);
-       }
+        //better syntax when derefencing a member from an iterator pointer (*it)->mem;
+        cout << (*it)->name << " is " << (*it)->age << " years old." << endl;
     }
 
-    for(const auto& word: result_vector)
+    for(auto& p: people)
     {
-        if(index > 0 && index % 9 ==0)
-            cout << "\n";
-        cout << word << " ";
-        ++index;
+        delete p;
     }
-    cout << '\n';
     return 0;
 }
-
-/*
- //const int* const ptr = &x;
-//  ^      ^
-//  |      |
-//  |      const pointer (CAN'T reassign)
-//  const int (CAN'T modify value)
-
-// This has BOTH:
-// 1. Low-level const (const int) - can't modify value
-// 2. High-level const (const ptr) - can't reassign pointer*/
